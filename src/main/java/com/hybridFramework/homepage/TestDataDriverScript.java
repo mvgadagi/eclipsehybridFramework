5f1563c37e7e4753d56f0084798e053e3cc79780
mvgadagi <mvgadagi@gmail.com>
2017-07-30 21:04:43 +0530

    On 7/30

--- src/main/java/com/hybridFramework/homepage/TestDataDriverScript.java
+++ src/main/java/com/hybridFramework/homepage/TestDataDriverScript.java
@@ -28,6 +28,6 @@ public class TestDataDriverScript extends TestBase {
 
 	@Test
 	public void skipMethod() {
-		System.out.println("Skipping Test!! ");
+		System.out.println("Skipping Test!!");
 	}
 }
